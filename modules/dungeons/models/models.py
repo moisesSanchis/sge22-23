d4444e33e2ddbd63b4066d79600ed7e6322c5466
moisessanchispla@gmail.com <moisessanchispla@gmail.com>
2022-11-07 20:57:08 +0100

    cambios realizados en casa 7 noviembre

--- modules/dungeons/models/models.py
+++ modules/dungeons/models/models.py
@@ -1,54 +1,82 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
 
 class player(models.Model):
     _name = 'dungeons.player'
     _description = 'players'
 
     name = fields.Char()
-    avatar = fields.Image(max_width = 200, max_height = 200) #Atributo avatar que es una imagen y tiene un tamaño predeterminado.
+    avatar = fields.Image(max_width=200,
+                          max_height=200)  # Atributo avatar que es una imagen y tiene un tamaño predeterminado.
     login = fields.Char()
     password = fields.Char()
-    heart_player = fields.One2many(comodel_name='dungeons.heart', inverse_name='player') #Relacion entre jugadores y mazmorras
+    heart_player = fields.One2many(comodel_name='dungeons.heart',
+                                   inverse_name='player')  # Relacion entre jugadores y mazmorras
 
-class heart(models.Model): 
+
+class heart(models.Model):
     _name = 'dungeons.heart'
     _description = 'Hearts'
-    
+
     name = fields.Char()
-    avatar_heart = fields.Image(max_width = 200, max_height = 200)
+    avatar_heart = fields.Image(max_width=200, max_height=200)
     player = fields.Many2one('dungeons.player', ondelete='cascade')
-    life = fields.Integer(default = 2000)
-    iron = fields.Integer(default = 100)
-    coal = fields.Integer(default = 50)
-    steel = fields.Integer(default = 10)
-    defense_creature = fields.Integer(default = 0)
-    magical_creature = fields.Integer(default = 0)
-    warrior_creature = fields.Integer(default = 0)
+    life = fields.Integer(default=2000)
+    iron = fields.Integer(default=100)
+    coal = fields.Integer(default=50)
+    steel = fields.Integer(default=10)
+    defense_creature = fields.Integer(default=0)
+    magical_creature = fields.Integer(default=0)
+    warrior_creature = fields.Integer(default=0)
     buildings = fields.One2many('dungeons.buildings', 'heart')
 
-    
+    @api.constrains('iron')
+    def _check_something(self):
+        for record in self:
+            if record.iron > 3000:
+                raise ValidationError("You have too much iron " % record.iron)
+
+    @api.constrains('coal')
+    def _check_something(self):
+        for record in self:
+            if record.coal > 3000:
+                raise ValidationError("You have too much coal " % record.coal)
+
+    @api.constrains('steel')
+    def _check_something(self):
+        for record in self:
+            if record.steel > 3000:
+                raise ValidationError("You have too much steel " % record.steel)
+
+
 class buildings(models.Model):
-    _name = 'dungeons.buildings' 
+    _name = 'dungeons.buildings'
     _description = 'Buildings'
-    
-    name = fields.Char() 
+
+    name = fields.Char()
     level = fields.Integer()
     heart = fields.Many2one('dungeons.heart', ondelete='restrict')
     building_type = fields.Many2one('dungeons.building_type', ondelete='restrict')
-    image_building = fields.Image(max_width = 200, max_height = 200, related = 'building_type.image_building')
-    production_iron = fields.Float(related = 'building_type.production_iron')
-    production_coal = fields.Float(related = 'building_type.production_coal')
-    production_steel = fields.Float(related = 'building_type.production_steel')
-    consume_iron = fields.Float(related = 'building_type.consume_iron')
-    consume_coal = fields.Float(related = 'building_type.consume_coal')
-    production_magical_creatures = fields.Float(related = 'building_type.production_magical_creatures')
-    production_warrior_creatures = fields.Float(related = 'building_type.production_warrior_creatures')
-    production_defense_creatures = fields.Float(related = 'building_type.production_defense_creatures')
-    
-    
- 
+    image_building = fields.Image(max_width=200, max_height=200, related='building_type.image_building')
+    production_iron = fields.Float(related='building_type.production_iron')
+    production_coal = fields.Float(related='building_type.production_coal')
+    production_steel = fields.Float(related='building_type.production_steel')
+    consume_iron = fields.Float(related='building_type.consume_iron')
+    consume_coal = fields.Float(related='building_type.consume_coal')
+    production_magical_creatures = fields.Float(related='building_type.production_magical_creatures')
+    production_warrior_creatures = fields.Float(related='building_type.production_warrior_creatures')
+    production_defense_creatures = fields.Float(related='building_type.production_defense_creatures')
+
+    @api.constrains('level')
+    def check_level(self):
+        for record in self:
+            if record.level > 15:
+                raise ValidationError("Level cant be more than 15" % record.level)
+
+
 class building_type(models.Model):
     _name = 'dungeons.building_type'
     _description = 'Building types'
